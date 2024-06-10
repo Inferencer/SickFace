263f700c69453d185232f854d3ab40153d02ecde
Inferencer <121839197+Inferencer@users.noreply.github.com>
2024-06-10 21:10:55 +0100

    auto open + demo fix
    due to the change in demo.py allowing multiple source images in console i had to edit the ui to reflect these changes, however i am keeping the ui as a single source image
    
    i have also added an auto open in browser

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import gradio as gr
 import os
+import moviepy.editor as mp
 
 def run_demo(input_image_path, driving_video_path, movement_scale):
     # Default paths for checkpoint and config
@@ -14,8 +15,13 @@ def run_demo(input_image_path, driving_video_path, movement_scale):
     else:
         raise ValueError("Invalid movement scale option")
     
+    # Limit driving video to 30 seconds
+    clip = mp.VideoFileClip(driving_video_path)
+    if clip.duration > 30:
+        raise ValueError("Driving video must be 30 seconds or less")
+    
     # Construct and execute the command
-    command = f'python demo.py --checkpoint {checkpoint_path} --config {config_path} --source_image {input_image_path} --driving_video {driving_video_path} --source_idx 0 {movement_scale_option} --adapt_scale --find_best_frame'
+    command = f'python demo.py --checkpoint {checkpoint_path} --config {config_path} --source_image {input_image_path} --driving_video {driving_video_path} {movement_scale_option} --adapt_scale --find_best_frame'
     os.system(command)
     
     # Path to the result video
@@ -31,13 +37,15 @@ def demo_ui(input_image_path, driving_video_path, movement_scale):
 
 input_image = gr.Image(label="Upload Input Image (512x512)", type="filepath")
 driving_video = gr.Video(label="Upload Driving Video (512x512)")
-movement_scale_option = gr.Radio(["Relative", "Absolute"], label="Movement Scale", value="Relative")
+movement_scale_option = gr.Radio(["Relative", "Absolute"], label="Animate", value="Relative")
 
 with gr.Blocks() as demo_interface:
     with gr.Row():
         input_image.render()
         driving_video.render()
     movement_scale_option.render()
+    warning_text = "Warning: We have limited the input driving video to 30 seconds. If the video is above 30 seconds, it will error. For full control, visit the repo at https://github.com/Inferencer/SickFace"
+    gr.Text(warning_text)
     generate_button = gr.Button("Generate")
     result_video = gr.Video(label="Result Video")
     
@@ -47,4 +55,4 @@ with gr.Blocks() as demo_interface:
         outputs=result_video
     )
 
-demo_interface.launch()
+demo_interface.launch(inbrowser=True)
